e544704caf0becef7e4d93001fc0757ce7a8e1c1
Judith <j.espinalm12@gmail.com>
2025-01-06 11:55:00 -0400

    Corrected betty style for main

--- main.h
+++ main.h
@@ -1,4 +1,4 @@
-#ifndef MAIN_H /* Include guard */
+#ifndef MAIN_H
 #define MAIN_H
 
 /* Include necessary libraries */
@@ -10,15 +10,10 @@
 #include <sys/wait.h>
 #include <errno.h>
 
-extern char **environ; /* Global environment variable */
+/* Declare external variables */
+extern char **environ;
 
 /* Function prototypes */
-/**
- * _fork.c - Creates a child process and executes a command.
- * @line: Input string containing the command.
- *
- * Return: PID of the child process, or -1 on failure.
- */
 int _fork(char *line);
 
 #endif /* MAIN_H */
