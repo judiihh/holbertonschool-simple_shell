484c341f14d703111255f44d87879603117839a5
adolfo2231 <adolfo.rodriguez2231@gmail.com>
2025-01-09 11:00:24 -0400

    add _isspace funtion

--- trim_spaces.c
+++ trim_spaces.c
@@ -1,6 +1,18 @@
 #include "main.h"
 
 /**
+ * _isspace - Checks if a character is a whitespace character.
+ * @c: The character to check.
+ *
+ * Return: 1 if the character is a whitespace, 0 otherwise.
+ */
+int _isspace(char c)
+{
+	return (c == ' ' || c == '\t' || c == '\n' || c == '\v' ||
+			c == '\f' || c == '\r');
+}
+
+/**
  * trim_spaces - Trims leading and trailing spaces from a string.
  * @line: The string to trim.
  *
@@ -11,7 +23,7 @@ char *trim_spaces(char *line)
 	char *start = line, *end;
 
 	/* Trim leading spaces */
-	while (*start && isspace((unsigned char)*start))
+	while (*start && _isspace(*start))
 		start++;
 
 	/* If the string is all spaces, return an empty string */
@@ -20,7 +32,7 @@ char *trim_spaces(char *line)
 
 	/* Trim trailing spaces */
 	end = start + strlen(start) - 1;
-	while (end > start && isspace((unsigned char)*end))
+	while (end > start && _isspace(*end))
 		end--;
 
 	*(end + 1) = '\0'; /* Null-terminate the trimmed string */
